6ef718d7c30a7a4b557b7547b12354d62871923b
Joe Hill <joe@jkh3.com>
2017-04-15 23:25:41 -0400

    Tracking more specific locations involving Country, City, State, and Postal Code

--- userLocation/ViewController.swift
+++ userLocation/ViewController.swift
@@ -39,28 +39,73 @@ class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDele
             
             let userLocation: CLLocation = locations[0]
             
-            let latitude = userLocation.coordinate.latitude
+            CLGeocoder().reverseGeocodeLocation(userLocation) { (placemarks, error) in
+                
+                
+                if error != nil {
+                    
+                    print(error)
+                    
+                } else {
+                    
+                    if let placemark = placemarks?[0] {
+                        
+                        var subThoroughfare = ""
+                        
+                        if placemark.subThoroughfare != nil {
+                            
+                            subThoroughfare = placemark.subThoroughfare!
+                        }
+                        
+                        var thoroughfare = ""
+                        
+                        if placemark.thoroughfare != nil {
+                            
+                            thoroughfare = placemark.thoroughfare!
+                        }
+                        
+                        var subLocality = ""
+                        
+                        if placemark.subLocality != nil {
+                            
+                            subLocality = placemark.subLocality!
+                        }
+                        
+                        var subAdministrativeArea = ""
+                        
+                        if placemark.subAdministrativeArea != nil {
+                            
+                            subAdministrativeArea = placemark.subAdministrativeArea!
+                        }
+                        
+                        var postalCode = ""
+                        
+                        if placemark.postalCode != nil {
+                            
+                            postalCode = placemark.postalCode!
+                        
+                        }
+                        
+                        var country = ""
+                        
+                        if placemark.country != nil {
+                            
+                            country = placemark.country!
+                        
+                        }
+                        
+                        print(subThoroughfare + thoroughfare + "\n" + subLocality + "\n" +
+                            subAdministrativeArea + "\n" + postalCode + "\n" + country)
+                
+                }
             
-            let longitude = userLocation.coordinate.longitude
-            
-            let latDelta: CLLocationDegrees = 0.05
-            
-            let lonDelta: CLLocationDegrees = 0.05
-            
-            let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
-            
-            let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-            
-            let region = MKCoordinateRegion(center: location, span: span)
-            
-            self.map.setRegion(region, animated: true)
-            
-            
+            }
+
         }
 
-        
-        
     }
+    
+}
     
     
 
